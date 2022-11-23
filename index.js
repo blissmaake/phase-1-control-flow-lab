db5e069c2034a8641037346c5898de22e482e293
Bliss <blissmaake140@gmail.com>
2022-11-23 12:28:40 +0300

    Done Assignment

--- index.js
+++ index.js
@@ -17,15 +17,16 @@ function ternaryCheckCity(city){
 }
 
 function switchOnCharmFromTip(tip){
-  if (tip === "generous"){
-    return 'Thank you so much.'
-  }  
-  else if (tip === "not as generous") {
-    return 'Thank you.'
+  switch(tip) {
+    case 'generous':
+      return "Thank you so much.";
+      break;
+      case 'not as generous':
+        return "Thank you.";
+        break;
+        default:
+          return "Bye.";
+          break;
   }
-  else {
-    return 'Bye.'
-  }
-  
   // Write your code here!
 }
